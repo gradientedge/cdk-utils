78272d270d14cd280b659369c766fad856678c37
srinidhi <srinidhi@tensible.io>
2021-02-05 15:17:07 +0000

    adding variant construct for CfnLogGroup

--- packages/@common-aws/lib/logManager.ts
+++ packages/@common-aws/lib/logManager.ts
@@ -8,7 +8,12 @@ export interface LogProps extends logs.CfnLogGroupProps {
 }
 
 export class LogManager {
-  public createLogGroup(id: string, key: string, scope: CommonConstruct, props: CommonStackProps) {
+  public createCfnLogGroup(
+    id: string,
+    key: string,
+    scope: CommonConstruct,
+    props: CommonStackProps
+  ) {
     if (!props.logs || props.logs.length == 0) throw `Logs props undefined`
 
     const logProps = props.logs.find((log: LogProps) => log.key === key)
@@ -20,6 +25,22 @@ export class LogManager {
     })
 
     createCfnOutput(`${id}Arn`, scope, logGroup.attrArn)
+
+    return logGroup
+  }
+
+  public createLogGroup(id: string, key: string, scope: CommonConstruct, props: CommonStackProps) {
+    if (!props.logs || props.logs.length == 0) throw `Logs props undefined`
+
+    const logProps = props.logs.find((log: LogProps) => log.key === key)
+    if (!logProps) throw `Could not find log props for key:${key}`
+
+    const logGroup = new logs.LogGroup(scope, `${id}`, {
+      logGroupName: `${logProps.logGroupName}-${props.stage}`,
+      retention: logProps.retentionInDays,
+    })
+
+    createCfnOutput(`${id}Arn`, scope, logGroup.logGroupArn)
 
     return logGroup
   }
