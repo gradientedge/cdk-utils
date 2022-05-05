f0dc530c62a84880269ce7bbb41fec7fdb946648
srinidhi <srinidhi@tensible.io>
2022-05-06 00:35:17 +0100

    feat(event-targets): workaround for CDK issue: https://github.com/aws/aws-cdk/issues/17002
    workaround for CDK issue: https://github.com/aws/aws-cdk/issues/17002

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     'no-empty': 'off'
   }
 }
---
+++ src/test/manager/event-target-manager.test.ts
@@ -0,0 +1,78 @@
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+import { Construct } from 'constructs'
+import * as common from '../../lib/common'
+import * as types from '../../lib/types'
+
+interface TestStackProps extends types.CommonStackProps {
+  testLogGroup: any
+}
+
+const testStackProps = {
+  env: {
+    account: '123456789',
+    region: 'eu-west-1',
+  },
+  name: 'test-common-stack',
+  domainName: 'gradientedge.io',
+  region: 'eu-west-1',
+  stackName: 'test',
+  stage: 'test',
+  extraContexts: ['src/test/common/cdkConfig/logs.json'],
+  stageContextPath: 'src/test/common/cdkEnv',
+}
+
+class TestCommonStack extends common.CommonStack {
+  declare props: TestStackProps
+
+  constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
+    super(parent, name, props)
+
+    this.construct = new TestCommonConstruct(this, testStackProps.name, this.props)
+  }
+
+  protected determineConstructProps(props: cdk.StackProps) {
+    return {
+      ...super.determineConstructProps(props),
+      ...{
+        testLogGroup: this.node.tryGetContext('testLogGroup'),
+      },
+    }
+  }
+}
+
+class TestCommonConstruct extends common.CommonConstruct {
+  declare props: TestStackProps
+
+  constructor(parent: Construct, name: string, props: TestStackProps) {
+    super(parent, name, props)
+    const testLogGroup = this.logManager.createLogGroup('test-log-group', this, this.props.testLogGroup)
+    this.eventTargetManager.createCloudWatchLogGroupNoPolicy('test-log-group-target', this, testLogGroup)
+  }
+}
+
+const app = new cdk.App({ context: testStackProps })
+const commonStack = new TestCommonStack(app, 'test-common-stack', testStackProps)
+const template = Template.fromStack(commonStack)
+
+describe('TestEventTargetConstruct', () => {
+  test('synthesises as expected', () => {
+    /* test if number of resources are correctly synthesised */
+    template.resourceCountIs('AWS::Logs::LogGroup', 1)
+    template.resourceCountIs('AWS::Logs::ResourcePolicy', 0)
+  })
+})
+
+describe('TestEventTargetConstruct', () => {
+  test('outputs as expected', () => {
+    template.hasOutput('testLogGroupLogGroupArn', {})
+  })
+})
+
+describe('TestEventTargetConstruct', () => {
+  test('provisions new log group target as expected', () => {
+    template.hasResourceProperties('AWS::Logs::LogGroup', {
+      LogGroupName: 'test-lg-test',
+    })
+  })
+})
