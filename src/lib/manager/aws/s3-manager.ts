fc7293fe1b0ac71e89f24784c2c53136dbed0dc7
srinidhi <srinidhi@tensible.io>
2022-09-19 18:00:16 +0100

    fix(s3): Shortened folder ID for construct limits

--- src/lib/manager/aws/s3-manager.ts
+++ src/lib/manager/aws/s3-manager.ts
@@ -214,7 +214,7 @@ export class S3Manager {
     }
 
     folders.forEach(folder => {
-      new s3deploy.BucketDeployment(scope, `${id}-${folder}`, {
+      new s3deploy.BucketDeployment(scope, `${folder}`, {
         destinationBucket: bucket,
         destinationKeyPrefix: folder,
         sources: [s3deploy.Source.data('README.md', `This is the ${folder} folder for ${id}`)],
