42da99f72e16d75e9c4bd3d7cee4982c35bf2944
Hemal Shah <hemal@gradientedge.com>
2023-10-11 19:39:10 +0300

    feat(dynamodb): table v2 constrcuts

--- src/lib/aws/services/dynamodb/types.ts
+++ src/lib/aws/services/dynamodb/types.ts
@@ -7,6 +7,4 @@ export interface TableProps extends dynamodb.TableProps {
   tags?: TagProps[]
 }
 
-export interface TablePropsV2 extends dynamodb.TablePropsV2 {
-  tags?: TagProps[]
-}
+export interface TablePropsV2 extends dynamodb.TablePropsV2 {}
