f63f78d52172407785f630263f425ab5b4dc1132
srinidhi <srinidhi@tensible.io>
2023-11-20 09:48:54 +0000

    feat(cloudflare-zone): adding support for Cloudflare Zone resources

--- src/lib/cloudflare/services/zone/types.ts
+++ src/lib/cloudflare/services/zone/types.ts
@@ -1,3 +1,19 @@
 import { ZoneConfig } from '@cdktf/provider-cloudflare/lib/zone'
+import { ZoneCacheReserveConfig } from '@cdktf/provider-cloudflare/lib/zone-cache-reserve'
+import { ZoneCacheVariantsConfig } from '@cdktf/provider-cloudflare/lib/zone-cache-variants'
+import { ZoneDnssecConfig } from '@cdktf/provider-cloudflare/lib/zone-dnssec'
+import { ZoneHoldConfig } from '@cdktf/provider-cloudflare/lib/zone-hold'
+import { ZoneLockdownConfig } from '@cdktf/provider-cloudflare/lib/zone-lockdown'
+import { ZoneSettingsOverrideConfig } from '@cdktf/provider-cloudflare/lib/zone-settings-override'
 
 export interface ZoneProps extends ZoneConfig {}
+export interface ZoneOptions {
+  id?: string
+  name?: string
+}
+export interface ZoneCacheReserveProps extends ZoneCacheReserveConfig {}
+export interface ZoneCacheVariantsProps extends ZoneCacheVariantsConfig {}
+export interface ZoneDnssecProps extends ZoneDnssecConfig {}
+export interface ZoneHoldProps extends ZoneHoldConfig {}
+export interface ZoneLockdownProps extends ZoneLockdownConfig {}
+export interface ZoneSettingsOverrideProps extends ZoneSettingsOverrideConfig {}
