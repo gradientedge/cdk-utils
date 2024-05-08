ee664fe53719c65f848c7b1219b28e2da920d0a0
Kamaz <kamaz@xsimple.org>
2024-05-08 15:18:58 +0100

    feat: use existing s3 and cloudfront to deploy static

--- src/lib/aws/construct/static-asset-deployment/main.ts
+++ src/lib/aws/construct/static-asset-deployment/main.ts
@@ -1,5 +1,6 @@
-import { IBucket } from 'aws-cdk-lib/aws-s3'
-import { BucketDeployment } from 'aws-cdk-lib/aws-s3-deployment'
+import { IBucket, Bucket } from 'aws-cdk-lib/aws-s3'
+import { IDistribution, Distribution } from 'aws-cdk-lib/aws-cloudfront'
+import { BucketDeployment, Source, BucketDeploymentProps, ISource } from 'aws-cdk-lib/aws-s3-deployment'
 import { Construct } from 'constructs'
 import _ from 'lodash'
 import { CommonConstruct } from '../../common'
@@ -27,8 +28,10 @@ export class StaticAssetDeployment extends CommonConstruct {
 
   /* construct resources */
   staticAssetBucket: IBucket
+  cloudfrontDistribution?: IDistribution
 
   constructor(parent: Construct, id: string, props: StaticAssetDeploymentProps) {
+    // default to create bucket to keep backward complatibility
     super(parent, id, props)
     this.props = props
     this.id = id
@@ -38,7 +41,11 @@ export class StaticAssetDeployment extends CommonConstruct {
    * @summary Initialise and provision resources
    */
   public initResources() {
-    this.createAssetBucket()
+    if (this.props.createBucket) {
+      this.createAssetBucket()
+    } else {
+      this.loadAssetBucket()
+    }
     this.deployStaticAssets()
   }
 
@@ -50,13 +57,68 @@ export class StaticAssetDeployment extends CommonConstruct {
   }
 
   /**
+   * @summary Loads the static asset bucket
+   */
+  protected loadAssetBucket() {
+    this.staticAssetBucket = Bucket.fromBucketName(
+      this,
+      `${this.id}-sa-bucket`,
+      this.props.staticAssetBucket.bucketName
+    )
+  }
+
+  /**
+   * @summary Distribute the load for the static asset bucket if both distribution and paths are provided
+   */
+  protected loadDistribution() {
+    if (
+      this.props.cloudFrontDistribution &&
+      this.props.cloudFrontDistribution.invalidationPaths &&
+      this.props.cloudFrontDistribution.invalidationPaths.length > 0
+    ) {
+      this.cloudfrontDistribution = Distribution.fromDistributionAttributes(
+        this,
+        `${this.id}-sa-distribution`,
+        this.props.cloudFrontDistribution
+      )
+    }
+  }
+
+  /**
    * @summary Deploy the static assets into the static asset bucket
    */
   protected deployStaticAssets() {
+    let sources: Array<ISource> = []
+    if (
+      Array.isArray(this.props.staticAssetSources) &&
+      this.props.staticAssetSources.length > 0 &&
+      typeof this.props.staticAssetSources[0] === 'string'
+    ) {
+      sources = this.props.staticAssetSources.map(source => Source.asset(source))
+    } else {
+      sources = this.props.staticAssetSources as ISource[]
+    }
+
+    let distributionOptions: Pick<BucketDeploymentProps, 'distribution' | 'distributionPaths'> = {}
+    if (this.cloudfrontDistribution) {
+      distributionOptions = {
+        distribution: this.cloudfrontDistribution,
+        distributionPaths: this.props.cloudFrontDistribution?.invalidationPaths,
+      }
+    }
+
+    let pruneOptions: Pick<BucketDeploymentProps, 'prune'> = {}
+    if (this.props.prune !== undefined) {
+      pruneOptions = {
+        prune: this.props.prune,
+      }
+    }
     new BucketDeployment(this, `${this.id}-static-deployment`, {
       ...this.props.staticAssetDeployment,
       destinationBucket: this.staticAssetBucket,
-      sources: this.props.staticAssetSources,
+      sources: sources,
+      ...distributionOptions,
+      ...pruneOptions,
     })
 
     const staticAssetsForExport = this.props.staticAssetsForExport
--- src/lib/aws/construct/static-asset-deployment/types.ts
+++ src/lib/aws/construct/static-asset-deployment/types.ts
@@ -1,3 +1,4 @@
+import { DistributionAttributes } from 'aws-cdk-lib/aws-cloudfront'
 import { BucketDeploymentProps, S3BucketProps } from '../../services'
 import { CommonStackProps } from '../../common'
 
@@ -7,8 +8,21 @@ export interface AssetExport {
 }
 
 export interface StaticAssetDeploymentProps extends CommonStackProps {
+  /**
+   * @summary If value set to `true` construct will create bucket otherwise it is going to resolve bucket from the name
+   *
+   * @default true
+   */
+  createBucket?: boolean
   staticAssetBucket: S3BucketProps
   staticAssetDeployment: BucketDeploymentProps
-  staticAssetSources: any[]
+  staticAssetSources: any[] | string[]
   staticAssetsForExport?: AssetExport[]
+  destinationKeyPrefix?: string
+  prune?: boolean
+  /**
+   * @summary The CloudFront distribution to associate with the bucket. When value is configured, the construct will invalidate the distribution after the deployment.
+   *
+   */
+  cloudFrontDistribution?: DistributionAttributes & { invalidationPaths: string[] }
 }
